63752fa53efabd932a9f7fa25d1e5330dc1cb37b
vince007 <13840148193@163.com>
2021-08-12 15:55:00 +0800

    qqq

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@
         this.$notify({
           title: 'It works!',
           type: 'success',
-          message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
+          message: 'We\'ve laid the ground work for youfddsd. It\'s time for you to build something epic!',
           duration: 5000
         })
       }
